1a28e03aad4174b215f0323ec7ea8349bd58c026
Huy Phan <huypm@onlinebizsoft.com>
2023-12-14 03:13:11 +0700

    new filters

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const fs = require('fs');
 fs.writeFileSync('./filters.txt', '');
 
 const ublockFilters = [
-	'www.youtube.com##ytd-rich-item-renderer:has(#details:has-text(//i))',
-	'www.youtube.com##ytd-video-renderer:has(.yt-simple-endpoint:has-text(//i))',
-	'www.youtube.com##ytd-channel-renderer:has(.yt-simple-endpoint:has-text(//i))',
-	'www.youtube.com##ytd-watch-flexy:has(#text-container:has-text(//i))',
+	'www.youtube.com##ytd-app:has(#above-the-fold:has-text(//i))',
+	'www.youtube.com##ytd-app:has(#inner-header-container:has-text(//i))',
+	'www.youtube.com##ytd-compact-video-renderer:has-text(//i)',
+	'www.youtube.com##ytd-rich-item-renderer:has-text(//i)',
+	'www.youtube.com##ytd-video-renderer:has-text(//i)',
+	'www.youtube.com###content-section:has-text(//i)',
 ];
 
 const allBlockTexts = fs.readFileSync('block-texts.txt', 'utf-8');
